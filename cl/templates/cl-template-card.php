bd7e7e9b3909f09335ee093102c4e59deedb44e3
Brandon Fuller <bjcfuller@uri.edu>
2017-05-23 12:11:36 -0400

    Made card shortcode templates a little more efficient

--- cl/templates/cl-template-card.php
+++ cl/templates/cl-template-card.php
@@ -1,18 +1,8 @@
 <?php if ($atts['clickable']): ?>
     <a class="card" href="<?php print $atts['link']; ?>">
-        <?php if ($atts['img']!=''): ?>
-        <img src="<?php print $atts['img']; ?>">
-        <?php endif; ?>
-        <?php if ($atts['title']!=''): ?>
-        <h1><?php print $atts['title']; ?></h1>
-        <?php endif; ?>
-        <?php if ($content!=null): ?>
-        <p><?php print $content; ?></p>
-        <?php endif; ?>
-        <span class="button"><?php print $atts['button']; ?></span>
-    </a>
 <?php else: ?>
     <div class="card">
+<?php endif; ?>
         <?php if ($atts['img']!=''): ?>
         <img src="<?php print $atts['img']; ?>">
         <?php endif; ?>
@@ -22,6 +12,13 @@
         <?php if ($content!=null): ?>
         <p><?php print $content; ?></p>
         <?php endif; ?>
+        <?php if ($atts['clickable']): ?>
+        <span class="button"><?php print $atts['button']; ?></span>
+        <?php else: ?>
         <a class="button" href="<?php print $atts['link']; ?>"><?php print $atts['button']; ?></a>
+        <?php endif; ?>
+<?php if ($atts['clickable']): ?>
+    </a>
+<?php else: ?>
     </div>
 <?php endif; ?>
--- cl/templates/cl-template-dcard.php
+++ cl/templates/cl-template-dcard.php
@@ -1,17 +1,8 @@
 <?php if ($atts['link']!=''): ?>
     <a class="dcard" href="<?php print $atts['link']; ?>">
-        <?php if ($atts['img']!=''): ?>
-        <img src="<?php print $atts['img']; ?>">
-        <?php endif; ?>
-        <?php if ($atts['title']!=''): ?>
-        <h1><?php print $atts['title']; ?></h1>
-        <?php endif; ?>
-        <?php if ($content!=null): ?>
-        <p><?php print $content; ?></p>
-        <?php endif; ?>
-    </a>
 <?php else: ?>
     <div class="dcard">
+<?php endif; ?>
         <?php if ($atts['img']!=''): ?>
         <img src="<?php print $atts['img']; ?>">
         <?php endif; ?>
@@ -21,5 +12,8 @@
         <?php if ($content!=null): ?>
         <p><?php print $content; ?></p>
         <?php endif; ?>
+<?php if ($atts['link']!=''): ?>
+    </a>
+<?php else: ?>
     </div>
 <?php endif; ?>
