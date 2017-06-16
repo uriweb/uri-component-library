f8976c31ceb8fe22992d0f66b95a6f4263cd5166
Brandon Fuller <bjcfuller@uri.edu>
2017-06-16 15:32:29 -0400

    Support multiple video heros per page; updated styles and some cleaner js

--- uri-component-library/js/cl.built.js
+++ uri-component-library/js/cl.built.js
@@ -1,27 +1,30 @@
 /* ======= HEROS ======= */
 
-var hero = {
-    poster:'',
-    vidID:'', 
-    player:''
-};
+var hero = [];
 
 
 /*
- * Get video ID and determine the API to load
+ * Get video ID(s) and load the API
  */
 $(function() {
-    hero.poster = document.querySelector('#hero .youtube');
-    if(hero.poster) { 
-        // Use the target img id as the vid ID
-        hero.vidID = hero.poster.getAttribute('id');
-        loadYouTubeAPI();
-    };
+    
+    document.querySelectorAll('.hero .poster').forEach(function(el) {
+
+        hero.push({
+            'poster' : el,
+            'parent' : el.parentNode,
+            'vidID' : el.getAttribute('id')
+        });
+        
+    });
+    
+    loadYouTubeAPI();
+    
 });
 
 
 /*
- * Load the YT API
+ * Load the API
  */
 function loadYouTubeAPI() {
     var tag = document.createElement('script');
@@ -32,43 +35,47 @@ function loadYouTubeAPI() {
   
 
 /*
- * Create the YT player
+ * Create the player
  */
 function onYouTubePlayerAPIReady() {
-    hero.player = new YT.Player(hero.vidID, {
-        width: '0',
-        height: '0',
-        videoId: hero.vidID,
-        playerVars: {
-            autoplay: 1,
-            //end: 30,
-            controls: 0,
-            showinfo: 0,
-            modestbranding: 1,
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange' : onPlayerStateChange,
-            'onError' : onPlayerError
-        }
-    });
     
-    $(window).resize(function(){
-        resizePlayer();
-    }); resizePlayer();
+    hero.forEach(function(el,i){
+        
+        el['player'] = new YT.Player(el.vidID, {
+            videoId: el.vidID,
+            playerVars: {
+                autoplay: 1,
+                controls: 0,
+                showinfo: 0,
+                modestbranding: 1,
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange' : onPlayerStateChange,
+                'onError' : onPlayerError(i)
+            }
+        });
+
+        $(window).resize(function(){
+            resizePlayer(i);
+        }); 
+        
+        resizePlayer(i);
+        
+    });
     
 }
 
 
 /*
- * Set the YT player height and position based on width
+ * Dynamically set the player height and position based on width
+ * @param int i the index of the hero to manipulate
  */
-function resizePlayer() {
-    var w = $('#hero').width();
-    var h = $('#hero').height();
-    console.log(w/h);
+function resizePlayer(i) {
+    var w = $(hero[i].parent).width(),
+        h = $(hero[i].parent).height();
     if (w/h > 16/9) {
-        $('#' + hero.vidID).css({
+        $('#' + hero[i].vidID).css({
             'height' : w * 9 / 16,
             'width' : '100%',
             'left' : 0,
@@ -76,7 +83,7 @@ function resizePlayer() {
         });
     } else {
         w = h * 16 / 9;
-        $('#' + hero.vidID).css({
+        $('#' + hero[i].vidID).css({
             'height' : '100%',
             'width' : w,
             'left' : 0 - w / 2,
@@ -87,19 +94,20 @@ function resizePlayer() {
 
 
 /*
- * Do things with the YT player when it's loaded
+ * Do things with the player when it's loaded
  */
 function onPlayerReady(event) {
     event.target.mute();
     playVideo(event);
 }
 
+
 /*
  * Get player state and decide what to do
  */
 function onPlayerStateChange(event) {
     var state = event.target.getPlayerState();
-    console.log(state);
+    //console.log('player state', state);
     switch (state) {
         case 0: // ended
             playVideo(event);
@@ -109,8 +117,9 @@ function onPlayerStateChange(event) {
     }
 }
 
+
 /*
- * Play the YT video
+ * Play the video
  */
 function playVideo(event) {
     event.target.playVideo();
@@ -119,9 +128,10 @@ function playVideo(event) {
 
 /*
  * Revert to poster if there's an error with the video
+ * @param int i the index of the hero to manipulate
 */
-function onPlayerError() {
-    $('#' + hero.vidID).replaceWith(hero.poster);
+function onPlayerError(i) {
+    $('#' + hero[i].vidID).replaceWith(hero[i].poster);
 }
 /* ======= MENUS ======= */
 
