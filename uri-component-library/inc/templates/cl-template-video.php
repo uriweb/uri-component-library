b142b9a54a090750ce7c078f5a855dcc6c9aaa98
Brandon Fuller <bjcfuller@uri.edu>
2017-08-24 16:01:09 -0400

    Fix some stupid errors in the video shortcode file; update plugin

--- uri-component-library/inc/templates/cl-template-video.php
+++ uri-component-library/inc/templates/cl-template-video.php
@@ -1,10 +1,10 @@
 <?php
 
 $output = '<div class="cl-video">';
-$output .= '<img id="' + $vid + '" src="' + $img + '"';
+$output .= '<img id="' . $vid . '" src="' . $img . '" alt="' . $alt . '"';
     
 if (!empty($aspect)) {
-    $output .= ' data-aspect="' + $aspect + '"';
+    $output .= ' data-aspect="' . $aspect . '"';
 }
 
 $output .= '>';
