8f0ea76c94333dacffdba11d76be08f96e7847f8
Brandon Fuller <bjcfuller@uri.edu>
2017-11-09 14:46:06 -0500

    Adjust social shortcode to ensure that at least one link must be set in order to build the component

--- uri-component-library/inc/templates/cl-template-social.php
+++ uri-component-library/inc/templates/cl-template-social.php
@@ -6,19 +6,27 @@ if(!empty($class)) {
     $classes .= ' ' . $class;
 }
 
-$output = '<ul class="' . $classes . '">';
+if(empty($facebook) && empty($instagram) && empty($twitter) && empty($linkedin)) {
+    
+    $output = '';
+    
+} else {
+    
+    $output = '<ul class="' . $classes . '">';
 
-if(!empty($facebook)) {
-    $output .= '<li><a href="' . $facebook . '" class="cl-social-facebook">Facebook</a></li>';
-}
-if(!empty($instagram)) {
-    $output .= '<li><a href="' . $instagram . '" class="cl-social-instagram">Instagram</a></li>';
-}
-if(!empty($twitter)) {
-    $output .= '<li><a href="' . $twitter . '" class="cl-social-twitter">Twitter</a></li>';
-}
-if(!empty($linkedin)) {
-    $output .= '<li><a href="' . $linkedin . '" class="cl-social-linkedin">LinkedIn</a></li>';
-}
+    if(!empty($facebook)) {
+        $output .= '<li><a href="' . $facebook . '" class="cl-social-facebook">Facebook</a></li>';
+    }
+    if(!empty($instagram)) {
+        $output .= '<li><a href="' . $instagram . '" class="cl-social-instagram">Instagram</a></li>';
+    }
+    if(!empty($twitter)) {
+        $output .= '<li><a href="' . $twitter . '" class="cl-social-twitter">Twitter</a></li>';
+    }
+    if(!empty($linkedin)) {
+        $output .= '<li><a href="' . $linkedin . '" class="cl-social-linkedin">LinkedIn</a></li>';
+    }
 
-$output .= '</ul>';
+    $output .= '</ul>';
+    
+}
