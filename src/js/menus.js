913895fcdce0ec4875fa53f71f11d353a74fa4fc
Brandon Fuller <bjcfuller@uri.edu>
2017-12-18 12:02:25 -0500

    - Stop uglifying js for debugging - Reorganize cl-menus script a bit and add dropdown triggers

--- src/js/menus.js
+++ src/js/menus.js
@@ -7,35 +7,49 @@
     window.addEventListener('load', initCLMenus, false);
     
     function initCLMenus() {
-        var i;
+        var uls, trigger, spans, i;
         
-        // Since we have JS, let's hide any nested menus
-        var uls = document.querySelectorAll('.cl-menu ul');
+        // Since we have JS, let's hide any nested menus and add submenu triggers
+        uls = document.querySelectorAll('.cl-menu ul');
         for (i=0; i<uls.length; i++) {
             uls[i].style.display = 'none';
+            trigger = document.createElement('span');
+            trigger.className = 'submenu-trigger';
+            trigger.innerHTML = 'Expand menu';
+            uls[i].parentNode.insertBefore(trigger, uls[i]);
         }
 
         // Append dropdown arrows, bind click event to submenu triggers, and control the submenu                               
-        var spans = document.querySelectorAll('.cl-menu span');
+        spans = document.querySelectorAll('.cl-menu .submenu-trigger');
         for(i=0; i<spans.length; i++) {
             var el = spans[i];
-            el.addEventListener('click', function() {
-                var ul = this.parentNode.querySelector('ul'),
-                    arrow = this.querySelector('.arrow');
-                
-                ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
-                
-                if (arrow.classList.contains('on')) {
-                    arrow.classList.remove('on');
-                } else {
-                    arrow.classList.add('on');
-                }
-                
-            });
+            
+            el.addEventListener('click', handleDropdown.bind(null, el));
             
             var arrow = document.createElement('div');
             arrow.className = 'arrow';
             el.appendChild(arrow);
+        }
+        
+    }
+    
+    
+    /*
+     * Control the submenu
+     * @param el el the submenu trigger clicked
+     */
+    function handleDropdown(el) {
+        
+        var ul, arrow;
+        
+        ul = el.parentNode.querySelector('ul');
+        ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+        
+        arrow = el.querySelector('.arrow');
+        if (arrow.classList.contains('on')) {
+            arrow.classList.remove('on');
+        } else {
+            arrow.classList.add('on');
         }
         
     }
