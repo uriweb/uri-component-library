aee9b95347a695aa4c6486b4c0dc10f0f2c90699
Brandon Fuller <bjcfuller@uri.edu>
2017-12-18 14:24:58 -0500

    Add js-specific css hook to menus; control submenus with mouseovers instead of clicks to preserve link function of submenu parents; add some styles for better submenu flow in horizontal menus

--- src/js/menus.js
+++ src/js/menus.js
@@ -4,54 +4,44 @@
     
     'use strict';
     
+    var hiddenClass = 'hidden';
+    
     window.addEventListener('load', initCLMenus, false);
     
     function initCLMenus() {
-        var uls, trigger, spans, i;
+        var menus, uls, trigger, spans, i;
         
         // Since we have JS, let's hide any nested menus and add submenu triggers
+        
+        menus = document.querySelectorAll('.cl-menu');
+        for (i=0; i<menus.length; i++) {
+            menus[i].classList.add('.cl-menu-js');
+        }
+        
         uls = document.querySelectorAll('.cl-menu ul');
         for (i=0; i<uls.length; i++) {
-            uls[i].style.display = 'none';
-            trigger = document.createElement('span');
-            trigger.className = 'submenu-trigger';
-            trigger.innerHTML = 'Expand menu';
-            uls[i].parentNode.insertBefore(trigger, uls[i]);
-        }
-
-        // Append dropdown arrows, bind click event to submenu triggers, and control the submenu                               
-        spans = document.querySelectorAll('.cl-menu .submenu-trigger');
-        for(i=0; i<spans.length; i++) {
-            var el = spans[i];
-            
-            el.addEventListener('click', handleDropdown.bind(null, el));
-            
-            var arrow = document.createElement('div');
-            arrow.className = 'arrow';
-            el.appendChild(arrow);
+            uls[i].classList.add(hiddenClass);
+            uls[i].parentNode.addEventListener('mouseover', mouseover.bind(null, uls[i]));
+            uls[i].parentNode.addEventListener('mouseout', mouseout.bind(null, uls[i]));
         }
         
     }
     
     
     /*
-     * Control the submenu
+     * Open the submenu
      * @param el el the submenu trigger clicked
      */
-    function handleDropdown(el) {
-        
-        var ul, arrow;
-        
-        ul = el.parentNode.querySelector('ul');
-        ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
-        
-        arrow = el.querySelector('.arrow');
-        if (arrow.classList.contains('on')) {
-            arrow.classList.remove('on');
-        } else {
-            arrow.classList.add('on');
-        }
-        
+    function mouseover(el) {  
+        el.classList.remove(hiddenClass);
+    }
+    
+    /*
+     * Close the submenu
+     * @param el el the submenu trigger clicked
+     */
+    function mouseout(el) {  
+        el.classList.add(hiddenClass); 
     }
     
 })();
