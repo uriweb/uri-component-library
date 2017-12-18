aaac017d163ab3dc667329d1df91b31aba3614c3
Brandon Fuller <bjcfuller@uri.edu>
2017-12-18 16:32:36 -0500

    Expand wp_nav_menu() function in menus shortcode template so it's easier to read/edit

--- inc/templates/cl-template-menu.php
+++ inc/templates/cl-template-menu.php
@@ -6,4 +6,14 @@ if(!empty($class)) {
     $classes .= ' ' . $class;
 }
 
-$output = wp_nav_menu( array('menu' => $name, 'menu_class' => 'cl-menu-list', 'depth' => $depth, 'container' => 'div', 'container_class' => $classes, 'container_id' => $id, 'fallback_cb' => false, 'echo' => false) );
+$output = wp_nav_menu( 
+    array(
+        'menu' => $name, 
+        'menu_class' => 'cl-menu-list', 
+        'depth' => $depth, 
+        'container' => 'div', 
+        'container_class' => $classes, 
+        'container_id' => $id, 
+        'fallback_cb' => false, 
+        'echo' => false
+    ) );
