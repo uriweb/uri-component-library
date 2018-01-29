a4b6d5c34bfcbfa081d2d82234b0a9049572f54e
Brandon Fuller <bjcfuller@uri.edu>
2018-01-29 14:18:42 -0500

    Bumped version number to 1.2.2

--- uri-component-library.php
+++ uri-component-library.php
@@ -3,7 +3,7 @@
 Plugin Name: URI Component Library
 Plugin URI: http://www.uri.edu
 Description: Component Library
-Version: 1.2.1
+Version: 1.2.2
 Author: URI Web Communications
 Author URI: 
 @author: Brandon Fuller <bjcfuller@uri.edu>
