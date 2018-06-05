8eb5d43e769941e68f712625aaa0b6336157374b
Brandon Fuller <bjcfuller@uri.edu>
2018-06-05 11:04:37 -0400

    Remove function to get url by id; get image srcset if image id is supplied

--- inc/cl-shortcodes.php
+++ inc/cl-shortcodes.php
@@ -34,44 +34,6 @@ function uri_cl_shortcode_get_template( $name ) {
 
 
 /**
- * Get the media ID from URL
- *
- * @see https://philipnewcomer.net/2012/11/get-the-attachment-id-from-an-image-url-in-wordpress/
- *
- * @param str $media_url the media url.
- */
-function uri_cl_get_media_id_from_url( $media_url = '' ) {
-
-	global $wpdb;
-	$media_id = false;
-
-	// If there is no url, return.
-	if ( '' == $media_url ) {
-		return;
-	}
-
-	// Get the upload directory paths
-	$upload_dir_paths = wp_upload_dir();
-
-	// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
-	if ( false !== strpos( $media_url, $upload_dir_paths['baseurl'] ) ) {
-
-		// If this is the URL of an auto-generated thumbnail, get the URL of the original image
-		$media_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $media_url );
-
-		// Remove the upload path base directory from the attachment URL
-		$media_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $media_url );
-
-		// Finally, run a custom database query to get the attachment ID from the modified attachment URL
-		$media_id = $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = %s AND wposts.post_type = 'attachment'", $media_url ) );
-
-	}
-
-	return $media_id;
-}
-
-
-/**
  * Boxout
  */
 function uri_cl_shortcode_boxout( $atts, $content = null ) {
--- inc/templates/cl-template-card.php
+++ inc/templates/cl-template-card.php
@@ -19,7 +19,19 @@ if ( ! empty( $css ) ) {
 $output .= '>';
 
 if ( ! empty( $img ) ) {
-	$output .= '<img src="' . $img . '" alt="' . $alt . '">';
+
+	$img_src = $img;
+	$img_srcset = '';
+
+	if ( is_numeric( $img ) ) {
+		$img_id = $img;
+		$img_src = wp_get_attachment_image_src( $img_id );
+		$img_meta = wp_get_attachment_metadata( $img_id );
+		$img_srcset = wp_calculate_image_srcset( array( $img_meta['width'], $img_meta['height'] ), $img_src, $img_meta );
+	}
+
+	$output .= '<img src="' . $img_src . '" srcset="' . $img_srcset . '" alt="' . $alt . '">';
+
 }
 
 if ( ! empty( $title ) || ! empty( $body ) ) {
