1641fcee47b7e1ca7c8ea6cb65699dba7798b715
John Pennypacker <jpennypacker@uri.edu>
2019-03-12 13:39:24 -0400

    passes image size from shortcode to component template

--- inc/display-posts/cl-display-posts-hero.php
+++ inc/display-posts/cl-display-posts-hero.php
@@ -5,4 +5,4 @@
  * @package uri-component-library
  */
 
-print do_shortcode( '[cl-hero headline="' . _uri_cl_escape_brackets( get_the_title() ) . '" subhead="' . _uri_cl_escape_brackets( get_the_excerpt() ) . '" link="' . get_the_permalink() . '" img="' . get_the_post_thumbnail_url() . '" button="Read More" ' . $cl_atts . ']' );
+print do_shortcode( '[cl-hero headline="' . _uri_cl_escape_brackets( get_the_title() ) . '" subhead="' . _uri_cl_escape_brackets( get_the_excerpt() ) . '" link="' . get_the_permalink() . '" img="' . get_the_post_thumbnail_url(NULL, $original_atts['image_size']) . '" button="Read More" ' . $cl_atts . ']' );
