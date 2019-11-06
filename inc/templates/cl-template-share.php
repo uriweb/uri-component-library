601d0f3f1640c167c13462659f9ae1a5ea73d03e
Brandon Fuller <bjcfuller@uri.edu>
2019-11-06 11:20:53 -0500

    Add styles for share component

--- inc/templates/cl-template-share.php
+++ inc/templates/cl-template-share.php
@@ -15,9 +15,12 @@ if ( ! empty( $atts['css'] ) ) {
 
 $output .= '>';
 
-$output .= '<span class="share-label">Share:</span>';
-$output .= '<a href="http://twitter.com/share?text=' . get_the_title() . '&amp;url=' . esc_url( get_the_permalink() ) . '" title="Share on Twitter" class="cl-share-twitter share-twitter" target="_blank" rel="nofollow noopener">Twitter</a>';
-$output .= '<a href="https://www.facebook.com/sharer/sharer.php?u=' . esc_url( get_the_permalink() ) . '" title="Share on Facebook" class="cl-share-facebook share-facebook" target="_blank" rel="nofollow noopener">Facebook</a>';
+$output .= '<div class="cl-share-label">Share:</div>';
+
+$output .= '<ul class="cl-share-buttons">';
+$output .= '<li><a href="http://twitter.com/share?text=' . get_the_title() . '&amp;url=' . esc_url( get_the_permalink() ) . '" title="Share on Twitter" class="cl-share-twitter share-twitter" target="_blank" rel="nofollow noopener">Twitter</a></li>';
+$output .= '<li><a href="https://www.facebook.com/sharer/sharer.php?u=' . esc_url( get_the_permalink() ) . '" title="Share on Facebook" class="cl-share-facebook share-facebook" target="_blank" rel="nofollow noopener">Facebook</a></li>';
+$output .= '</ul>';
 
 $output .= '</div>';
 $output .= '</aside>';
