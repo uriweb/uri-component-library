fee7236847e5f1ff910462e3ec45b0bfaad77a04
John Pennypacker <jpennypacker@uri.edu>
2019-11-29 11:39:48 -0500

    removes "hero2"  (heroes using templates)

--- js/blocks/src/blocks.js
+++ js/blocks/src/blocks.js
@@ -2,7 +2,7 @@ import './boxout/block';
 import './button/block';
 import './card/block';
 import './hero/block';
-import './hero2/block';
+// import './hero2/block';
 import './metric/block';
 import './notice/block';
 import './panel/block';
