367003fad4106de12641254752c6d1fe6fc8fb5c
Brandon Fuller <bjcfuller@uri.edu>
2020-01-09 15:50:24 -0500

    Lint notice wysiwyg

--- js/wysiwyg/cl-notice-plugin.js
+++ js/wysiwyg/cl-notice-plugin.js
@@ -5,21 +5,19 @@
  * @package uri-component-library
  */
 
-(function() {
-
-	var cName = 'cl-notice',
+( function() {
+	const cName = 'cl-notice',
 		wName = 'CLNotice';
 
 	function generateNoticeShortcode( params ) {
-
-		var attributes = [], i;
+		let i;
+		const attributes = [];
 
 		for ( i in params ) {
-			attributes.push( i + '="' + URIWYSIWYG.htmlEscape( params[i] ) + '"' );
+			attributes.push( i + '="' + URIWYSIWYG.htmlEscape( params[ i ] ) + '"' );
 		}
 
 		return '[' + cName + ' ' + attributes.join( ' ' ) + ']' + params.content + '[/' + cName + ']';
-
 	}
 
 	tinymce.create(
@@ -33,82 +31,75 @@
 			 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
 			 * @param {string} url Absolute URL to where the plugin is located.
 			 */
-			init: function( ed, url ) {
-
+			init( ed, url ) {
 				// Add the button that the WP plugin defined in the mce_buttons filter callback
 				ed.addButton(
-				wName,
+					wName,
 					{
 						title: 'Notice',
 						text: '',
 						cmd: wName,
-						image: URIWYSIWYG.getPluginInfo().path + 'i/icons/notice.png'
-				}
+						image: URIWYSIWYG.getPluginInfo().path + 'i/icons/notice.png',
+					}
 				);
 
 				// Add a js callback for the button
 				ed.addCommand(
-				wName,
+					wName,
 					function( target, args ) {
-
-						var possibleArgs;
-
 						// Create an empty object if args is empty
 						if ( ! args ) {
 							args = {};
 						}
 
 						// Create an empty property so nothing is null
-						possibleArgs = ['title', 'content', 'style', 'class'];
+						const possibleArgs = [ 'title', 'content', 'style', 'class' ];
 						possibleArgs.forEach(
-						function( i ) {
-							if ( ! args[i] ) {
-								args[i] = '';
+							function( i ) {
+								if ( ! args[ i ] ) {
+									args[ i ] = '';
+								}
 							}
-						}
 						);
 
 						// Prevent nested quotes... escape / unescape instead?
 						args = URIWYSIWYG.unEscapeQuotesDeep( args );
 
 						ed.windowManager.open(
-						{
-							title: 'Insert / Update Notice',
-							body: [
-							{ type: 'textbox', name: 'title', label: 'Title', value: args.title },
-							{ type: 'textbox', multiline: 'true', name: 'content', label: 'Content', value: args.content, rows: 7, style: '' },
-							{ type: 'textbox', name: 'class', id: 'class', value: args.class, subtype: 'hidden' },
-							{ type: 'listbox', name: 'style', label: 'Style', value: args.style, 'values': [
-								{ text: 'Default', value: '' },
-								{ text: 'Urgent', value: 'urgent' }
-								]
-							}
-							],
-							onsubmit: function( e ) {
-
+							{
+								title: 'Insert / Update Notice',
+								body: [
+									{ type: 'textbox', name: 'title', label: 'Title', value: args.title },
+									{ type: 'textbox', multiline: 'true', name: 'content', label: 'Content', value: args.content, rows: 7, style: '' },
+									{ type: 'textbox', name: 'class', id: 'class', value: args.class, subtype: 'hidden' },
+									{ type: 'listbox', name: 'style', label: 'Style', value: args.style, values: [
+										{ text: 'Default', value: '' },
+										{ text: 'Urgent', value: 'urgent' },
+									],
+									},
+								],
+								onsubmit( e ) {
 								// Insert content when the window form is submitted
-								shortcode = generateNoticeShortcode( e.data );
-								ed.execCommand( 'mceInsertContent', 0, shortcode );
-
+									shortcode = generateNoticeShortcode( e.data );
+									ed.execCommand( 'mceInsertContent', 0, shortcode );
+								},
+							},
+							{
+								wp,
 							}
-						},
-						{
-							wp: wp
-						}
 						);
-
 					}
 				);
 
 				ed.on(
-				 'BeforeSetContent',
+					'BeforeSetContent',
 					function( event ) {
 						event.content = URIWYSIWYG.replaceShortcodes( event.content, cName, false, ed );
 					}
 				);
 
 				ed.on(
-				 'PostProcess',
+					'PostProcess',
 					function( event ) {
 						if ( event.get ) {
 							event.content = URIWYSIWYG.restoreShortcodes( event.content, cName );
@@ -118,12 +109,11 @@
 
 				// Open popup on placeholder double click
 				ed.on(
-				'DblClick',
+					'DblClick',
 					function( event ) {
 						URIWYSIWYG.openPopup( event.target, ed, cName, wName );
 					}
 				);
-
 			},
 
 			/**
@@ -132,14 +122,13 @@
 			 *
 			 * @return {Object} Name/value array containing information about the plugin.
 			 */
-			getInfo: function() {
+			getInfo() {
 				return URIWYSIWYG.getPluginInfo();
-			}
+			},
 
-	}
-		);
+		}
+	);
 
 	// Register plugin
 	tinymce.PluginManager.add( 'uri_cl_wysiwyg_notice', tinymce.plugins.uri_cl_wysiwyg_notice );
-
-})();
+}() );
