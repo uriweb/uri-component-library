f98b494991b50244a9b1de9d1e2b58d789cd2b30
Brandon Fuller <bjcfuller@uri.edu>
2020-01-09 11:51:58 -0500

    Lint menus.js

--- src/js/menus.js
+++ src/js/menus.js
@@ -5,25 +5,24 @@
  */
 
 ( function() {
-
 	'use strict';
 
-	var revealClass = 'reveal';
+	const revealClass = 'reveal';
 
 	window.addEventListener( 'load', initCLMenus, false );
 
 	function initCLMenus() {
-		var menus, toggle, list, uls, i, a, span, li;
+		let toggle, list, i, a, span, li;
 
 		// Since we have JS, let's add a unique css hook and submenu triggers
-		menus = document.querySelectorAll( '.cl-menu' );
+		const menus = document.querySelectorAll( '.cl-menu' );
 		for ( i = 0; i < menus.length; i++ ) {
 			toggle = document.createElement( 'span' );
 			toggle.className = 'cl-menu-toggle';
-			if ( '0' === menus[i].getAttribute( 'data-show-title' ) ) {
+			if ( '0' === menus[ i ].getAttribute( 'data-show-title' ) ) {
 				toggle.classList.add( 'hidden' );
 			}
-			toggle.innerHTML = menus[i].getAttribute( 'data-name' );
+			toggle.innerHTML = menus[ i ].getAttribute( 'data-name' );
 			toggle.addEventListener(
 				'click',
 				function() {
@@ -33,33 +32,31 @@
 						this.classList.add( 'active' );
 					}
 				}
-				);
+			);
 
-			list = menus[i].querySelector( '.cl-menu-list' );
+			list = menus[ i ].querySelector( '.cl-menu-list' );
 			list.classList.remove( 'cl-menu-list-no-js' );
 
-			menus[i].insertBefore( toggle, list );
-			menus[i].classList.add( 'cl-menu-js' );
+			menus[ i ].insertBefore( toggle, list );
+			menus[ i ].classList.add( 'cl-menu-js' );
 		}
 
-		uls = document.querySelectorAll( '.cl-menu-list ul' );
+		const uls = document.querySelectorAll( '.cl-menu-list ul' );
 		for ( i = 0; i < uls.length; i++ ) {
-
 			span = document.createElement( 'span' );
 			span.className = 'cl-menu-arrow';
-			uls[i].parentNode.append( span );
+			uls[ i ].parentNode.append( span );
 
-			a = uls[i].parentNode.querySelector( 'a' );
+			a = uls[ i ].parentNode.querySelector( 'a' );
 
 			li = document.createElement( 'li' );
 			li.appendChild( a.cloneNode( true ) );
-			uls[i].insertBefore( li, uls[i].firstChild );
+			uls[ i ].insertBefore( li, uls[ i ].firstChild );
 
 			a.classList.add( 'cl-menu-trigger' );
 
-			bindListeners( a, uls, uls[i] );
+			bindListeners( a, uls, uls[ i ] );
 		}
-
 	}
 
 	function bindListeners( a, uls, ul ) {
@@ -69,22 +66,19 @@
 				e.preventDefault();
 				handleClick( uls, ul );
 			}
-			);
+		);
 	}
 
 	function handleClick( els, el ) {
-		var toggle, i;
-
-		toggle = el.classList.contains( revealClass );
+		let i;
+		const toggle = el.classList.contains( revealClass );
 
 		for ( i = 0; i < els.length; i++ ) {
-			els[i].classList.remove( revealClass );
+			els[ i ].classList.remove( revealClass );
 		}
 
 		if ( ! toggle ) {
 			el.classList.add( revealClass );
 		}
-
 	}
-
-})();
+}() );
