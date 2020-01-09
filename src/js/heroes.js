2a647bbababdc083e09abf02e55e6979be1fa77b
Brandon Fuller <bjcfuller@uri.edu>
2020-01-09 11:51:04 -0500

    Lint heroes.js

--- src/js/heroes.js
+++ src/js/heroes.js
@@ -4,10 +4,9 @@
  * @package uri-component-library
  */
 
-var CLResizeSuperheroes;
+let CLResizeSuperheroes;
 
 ( function() {
-
 	'use strict';
 
 	window.addEventListener( 'load', initCLHeroes, false );
@@ -23,29 +22,25 @@ var CLResizeSuperheroes;
 	 * Animation control for animated image heroes
 	 */
 	function animate() {
+		let i;
 
-		var heroes, i;
-
-		heroes = document.querySelectorAll( '.cl-hero .animate.shift' );
+		const heroes = document.querySelectorAll( '.cl-hero .animate.shift' );
 
 		for ( i = 0; i < heroes.length; i++ ) {
-			heroes[i].style.backgroundPositionX = '100%';
-			heroes[i].style.backgroundPositionY = 0;
+			heroes[ i ].style.backgroundPositionX = '100%';
+			heroes[ i ].style.backgroundPositionY = 0;
 		}
-
 	}
 
 	/*
 	 * Blur header hero on scroll
 	 */
 	function blurHeroControl() {
+		let overlay, scroll, offset, radius;
 
-		var hero, overlay, scroll, offset, radius;
-
-		hero = document.getElementById( 'hero' );
+		const hero = document.getElementById( 'hero' );
 
 		if ( hero ) {
-
 			overlay = hero.querySelector( '.overlay' );
 			scroll = window.pageYOffset;
 			offset = overlay.getBoundingClientRect().top + scroll;
@@ -59,76 +54,67 @@ var CLResizeSuperheroes;
 					blurHero();
 				}
 			);
-
 		}
 
 		function blurHero() {
-			var p = window.pageYOffset,
+			const p = window.pageYOffset,
 				h = overlay.offsetHeight + offset,
 				b = Math.min( p / h * radius, radius );
 
 			overlay.style.webkitBackdropFilter = 'blur(' + b + 'px)';
 		}
-
 	}
 
 	function mobile() {
-
-		var els;
+		let els;
 
 		if ( URICL.checkSupport() ) {
-
 			els = document.querySelectorAll( '.cl-hero .poster[data-platform=youtube]' );
 
 			window.addEventListener(
 				'resize',
 				function() {
-					var i, w = window.innerWidth;
+					let i;
+					const w = window.innerWidth;
 					if ( w < 750 ) {
 						for ( i = 0; i < els.length; i++ ) {
-							els[i].classList.remove( 'unveil' );
+							els[ i ].classList.remove( 'unveil' );
 						}
 					} else {
 						for ( i = 0; i < els.length; i++ ) {
-							els[i].classList.add( 'unveil' );
+							els[ i ].classList.add( 'unveil' );
 						}
 					}
 				}
 			);
-
 		}
 	}
 
 	function superhero() {
-
-		var H = [], els, n, i;
-
-		els = document.querySelectorAll( '.cl-hero.super .cl-hero-proper' );
-		n = els.length;
+		let i;
+		const H = [];
+		const els = document.querySelectorAll( '.cl-hero.super .cl-hero-proper' );
+		const n = els.length;
 
 		for ( i = 0; i < n; i++ ) {
-
 			// Save a list of superheroes and their initial offsets
 			H.push(
-				 {
-						el: els[i],
-						offset: els[i].getBoundingClientRect().top
-			}
-				);
-
+				{
+					el: els[ i ],
+					offset: els[ i ].getBoundingClientRect().top,
+				}
+			);
 		}
 
 		CLResizeSuperheroes = function( getOffset ) {
+			let h, p;
 
-			var vh, vw, s, i, h, p;
-
-			vh = window.innerHeight;
-			vw = window.innerWidth;
-			s = window.pageYOffset;
+			const vh = window.innerHeight;
+			const vw = window.innerWidth;
+			const s = window.pageYOffset;
 
 			for ( i = 0; i < n; i++ ) {
-
-				h = H[i];
+				h = H[ i ];
 
 				if ( false !== getOffset ) {
 					h.offset = h.el.getBoundingClientRect().top;
@@ -144,17 +130,13 @@ var CLResizeSuperheroes;
 				if ( ( vh < vw * 0.75 && vh < 675 ) || ( p > 0.3 ) ) {
 					h.el.style.height = Math.max( 0.9 * vh, 500 ) + 'px';
 				} else {
-
 					/**
 					 * ELSE set the hero height to 98% of the browser height,
 					 * less the distance between the top of the page and the top of the hero
 					 */
-					h.el.style.height = 98 - p * 100 + 'vh';
-
+					h.el.style.height = 98 - ( p * 100 ) + 'vh';
 				}
-
 			}
-
 		};
 
 		// Trigger scroll event to refresh pageYOffset value on page reload (otherwise it'll be 0).
@@ -165,7 +147,5 @@ var CLResizeSuperheroes;
 
 		// Listen for any browser resizing.
 		window.addEventListener( 'resize', CLResizeSuperheroes.bind( null, false ), false );
-
 	}
-
-})();
+}() );
