f104d3c048e567dde4e8c3ba3ae2f5f77e632eda
Brandon Fuller <bjcfuller@uri.edu>
2020-10-21 15:18:01 -0400

    Fix issue with invalid block content

--- inc/block-patterns/hero-in-stage.php
+++ inc/block-patterns/hero-in-stage.php
@@ -7,14 +7,10 @@
 
 	$pattern = <<<PATTERN
 <!-- wp:group -->
-<div class="wp-block-group"><div class="wp-block-group__inner-container"><!-- wp:html -->
-<div id="stage" class="fade">
-<!-- /wp:html -->
+<div id="stage" class="wp-block-group fade"><div class="wp-block-group__inner-container">
 
-<!-- wp:uri-cl/hero {"headline":"Think Big","subhead":"Study in a small beautiful place with some of the biggest thinkers in the world.","link":"https://www.uri.edu/","id":"67f7251e-3f65-a8e2-42c5-6b183dddc040","vid":"https://vimeo.com/377092097/13e9ce1d78","img":"https://www.uri.edu/wp-content/uploads/home/2018/05/kingston-campus.jpg","button":"Learn More","format":"super"} /-->
+<!-- wp:uri-cl/hero {"headline":"Think Big","subhead":"Study in a small beautiful place with some of the biggest thinkers in the world.","link":"https://www.uri.edu/","id":"67f7251e-3f65-a8e2-42c5-6b183dddc040","vid":"https://vimeo.com/377092097/13e9ce1d78","img":"https://www.uri.edu/wp-content/uploads/home/2018/05/kingston-campus.jpg","button":"Learn More","format":"super","className":"fitted"} /-->
 
-<!-- wp:html -->
-</div>
-<!-- /wp:html --></div></div>
+</div></div>
 <!-- /wp:group -->
 PATTERN;
