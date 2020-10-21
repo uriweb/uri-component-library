2b0087528f3fab62e9ab8d58a905ea92ddcd94d9
Brandon Fuller <bjcfuller@uri.edu>
2020-10-21 16:02:44 -0400

    Update devDependencies
    Update devDependencies including webpack (major version change to v5.x); 
    recompile.  Webpack v5 has breaking changes but other than a syntax 
    error in the config file, it does not seem to be causing any issues.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const plugins = [];
 
 module.exports = {
 	context: __dirname,
-	devtool: debug ? 'inline-sourcemap' : null,
+	devtool: debug ? 'inline-source-map' : null,
 	mode: debug ? 'development' : 'production',
 	entry: './src/js/blocks/blocks.js',
 	output: {
