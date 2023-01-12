ebe2bd8c6b067f56c454d9f4b254a613a60b55d9
Brandon Fuller <bjcfuller@uri.edu>
2023-01-12 10:38:54 -0500

    Conditionally include the link

--- inc/display-posts/cl-display-posts-notice.php
+++ inc/display-posts/cl-display-posts-notice.php
@@ -7,7 +7,11 @@
 
 $sc = '[cl-notice title="' . _uri_cl_escape_brackets( get_the_title() ) . '" ' . $cl_atts . ']';
 $sc .= _uri_cl_escape_brackets( get_the_excerpt() );
-$sc .= '<p><a href="' . get_the_permalink() . '">Read more</a></p>';
+
+if ( "true" == $original_atts['include_link'] ) {
+    $sc .= '<p><a class="cl-notice-display-post-link" href="' . get_the_permalink() . '">Read more</a></p>';
+}
+
 $sc .= '[/cl-notice]';
 
 print do_shortcode( $sc );
