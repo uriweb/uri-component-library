2e5f04282058f3f84482a3ed7028a592db0f4d5a
Sarah Pucino <sarah.pucino@uri.edu>
2026-01-30 15:20:15 -0500

    add logic for eyebrow

--- inc/templates/cl-template-circle-card.php
+++ inc/templates/cl-template-circle-card.php
@@ -17,7 +17,7 @@ $output .= '<div id="circle">';
 $output .= '<div class="cl-circle-card-img" style="background-image:url(' . $atts['img'] . ')"></div>';
 
 $output .= '<div class="cl-circle-card-content">';
-$output .= '<p id="eyebrow">Impacting success</p>';
+$output .= '<p id="eyebrow">' . $atts['eyebrow'] . '</p>';
 $output .= '<hr id="liner">';
 $output .= '<h2>' . $atts['title'] . '</h2>';
 $output .= '<p>' . $atts['body'] . '</p>';                  
